0ce3cb5fda16be5338f7374cc52b76335310a591
Jeff Cho <mail@jeffcho.com>
2019-01-05 19:15:30 -0700

    Add nick and setname commands.

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext.commands import Bot
 from config import CONFIG
 from secure import SECURE
-from utility import is_admin, find_channel, find_role, user_has_role
+from utility import is_admin, is_mod, find_channel, \
+                    find_role, send_notification, user_has_role
 
 megabot = Bot(command_prefix = CONFIG['Prefix'])
 
@@ -19,7 +20,6 @@ async def on_member_join(member):
   rules_channel = find_channel(member.guild, CONFIG['RulesChannel'])
 
   new_role = find_role(member.guild, CONFIG['NewRole'])
-  confirmed_role = find_role(member.guild, CONFIG['ConfirmedRole'])
 
   await member.add_roles(new_role)
   await welcome_channel.send( 'Hello and welcome to the Megachannel, {0}! '.format(member.name) +
@@ -43,7 +43,7 @@ async def agree(ctx):
 
   if is_new and CONFIG['WelcomeChannel'] in ctx.message.channel.name:
     await member.remove_roles(new_role)
-    await notifications_channel.send('Please welcome our newest member {0} to the Megachannel!'.format(member.name))
+    await send_notification(ctx.guild, 'Please welcome our newest member {0} to the Megachannel!'.format(member.name))
     await member.send('You have agreed to the rules of the Megachannel! Please make sure you check back often ' +
                       'to keep up-to-date with changes.' +
                       '\n\nYou can now use any publicly-available channel; for example, you don\'t have to ' +
@@ -69,8 +69,51 @@ async def agree(ctx):
 # Command: get profile
 
 # Command: admin setname
+@megabot.command()
+async def setname(ctx, user, name):
+  """Admin command: change a user's nickname on this server.
+
+  Args:
+    user (Member):
+      The user whose nickname will be updated.
+
+    name (String):
+      String to set the user's nickname to - must be wrapped in
+      quotation marks if longer than one word.
+  """
+  if is_admin(ctx.author) or is_mod(ctx.author):
+
+    if len(ctx.message.mentions) == 0:
+      await ctx.send("`!setname`: The user whose name you wish to change must be mentioned as the first argument.")
+      return
+
+    target_user = ctx.message.mentions[0]
+    old_name = target_user.name
+    await target_user.edit(nick = name)
+    await send_notification(ctx.guild, "{} updated {}'s nickname to {}!".format(ctx.author.name, old_name, name))
+
+  else:
+
+    await ctx.send("`!setname`: You do not have the privileges to use this command.")
+
 
 # Command: user setname
+@megabot.command()
+async def nick(ctx, name):
+  """Change your nickname on this server.
+
+  Args:
+    name (String)
+      String that you want the nickname set to - must be wrapped in
+      quotation marks if longer than one word.
+
+  Returns:
+    None
+  """
+  old_name = ctx.author.name
+  await ctx.author.edit(nick = name)
+  await send_notification(ctx.guild, "{} updated their nickname to {}!".format(old_name, name))
+
 
 # Command: cap
 
@@ -87,7 +130,7 @@ async def echo(ctx, message):
   Returns:
     None
   """
-  if is_admin(ctx):
+  if is_admin(ctx.author):
     await ctx.send(message)
 
 
--- utility.py
+++ utility.py
@@ -3,32 +3,32 @@ from config import CONFIG
 admin_role_identifier = CONFIG['AdminRole']
 mod_role_identifier = CONFIG['ModRole']
 
-def is_admin(ctx):
-  """Check whether the author of a message is an admin.
+def is_admin(user):
+  """Check whether a user is an admin.
 
   Args:
-    ctx (Context)
-      The Context of the message which is being examined.
+    user (Member)
+      The user to check for admin privileges.
 
   Returns:
     is_admin (Bool)
     True if the message author has the admin role.
   """
-  return user_has_role(ctx.author, admin_role_identifier)
+  return user_has_role(user, admin_role_identifier)
 
 
-def is_mod(ctx):
-  """Check whether the author of a message is a moderator.
+def is_mod(user):
+  """Check whether a user is a moderator.
 
   Args:
-    ctx (Context)
-      The Context of the message which is being examined.
+    user (Member)
+      The user to check for mod privileges.
 
   Returns:
     is_mod (Bool)
     True if the message author has the mod role.
   """
-  return user_has_role(ctx.author, mod_role_identifier)
+  return user_has_role(user, mod_role_identifier)
 
 
 def find_channel(guild, channel_string):
@@ -98,3 +98,16 @@ def user_has_role(user, role_string):
       break
 
   return has_role
+
+async def send_notification(guild, message):
+  """Send a message to the notifications channel.
+
+  Args:
+    message (String)
+      The message to send to the notifications channel.
+
+  Returns:
+    None
+  """
+  notifications_channel = find_channel(guild, CONFIG['NotificationsChannel'])
+  await notifications_channel.send(message)
